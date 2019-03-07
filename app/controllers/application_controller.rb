97b61c19127501892ca35325efd90f29f5734e9d
Christina Williams <christinaxtwilliams@gmail.com>
2019-03-07 15:38:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < Sinatra::Base
-  
-  configure do
-    enable :sessions
-    register Sinatra::Flash
-    register Sinatra::ActiveRecordExtension
-    set :session_secret, "my_application_secret"
-    set :views, Proc.new { File.join(root, "../views/") }
-  endviews, Proc.new { File.join(root, "../views/") }
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
     erb :index
